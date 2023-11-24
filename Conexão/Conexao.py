bb25d5bcedef5f5005ecc87d8a44b97b4cdd5756
brendonwallacee <brendonwallace2001@gmail.com>
2023-11-24 01:30:15 -0300

    Ajuste no READ.ME e criação dos pacotes e classe Conexão

---
+++ Conexão/Conexao.py
@@ -0,0 +1,28 @@
+import pyodbc
+
+
+class Conexao:
+    def _init_(self):
+        pass
+
+    # Criação da função para abrir conexão com o BD SQL Server
+    @staticmethod
+    def getConnection():
+        try:
+            conn_str = 'DRIVER={SQL Server};SERVER=LAPTOP-BJQUB3C4\BD_CEOOP;DATABASE=CEOOP;'
+            return pyodbc.connect(conn_str)
+        except Exception as e:
+            raise Exception(str(e))
+
+    # Criação da função para fechar conexão com o BD SQL Server
+    @staticmethod
+    def closeConnection(conn, stmt=None, rs=None):
+        try:
+            if rs:
+                rs.close()
+            if stmt:
+                stmt.close()
+            if conn:
+                conn.close()
+        except Exception as e:
+            raise Exception(str(e))
